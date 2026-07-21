204a78c46f40ebf600b9002e03bf676bd08e2dea
siddharthsingh-glitch <siddharthsingh09062008@gmail.com>
2026-07-21 10:39:01 +0000

    Create driving license form program

---
+++ Driving_license_form.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <ctype.h>
+
+int main(void)
+{
+    string Name = get_string("What is your name? ");
+    printf("(Name: %s)\n", Name);
+
+    int Age = get_int("What is your age? ");
+    if(Age >= 18){
+        printf("(Age: %i)\n", Age);
+    } 
+    else{
+        printf("You are not eligible for Driving License");
+    }
+    
+    long Phone_Number = get_long("Write your phone number ");
+    printf("(Phone Number: %li)\n", Phone_Number);
+
+    char Pan_Number = get_char("Write your Pan card number ");
+    printf("(Pan Number: %c)\n", Pan_Number);
+
+    
+}
